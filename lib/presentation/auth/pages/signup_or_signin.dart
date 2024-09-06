3d46697874b73556ba0794d7f88c4bed31cec9db
Maxime Biechy <biechymaxime@gmail.com>
2024-09-06 13:13:55 +0200

    - Add : Sign In and SignUp Page

---
+++ lib/presentation/auth/pages/signup_or_signin.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_app/common/helpers/is_dark_mode.dart';
+import 'package:spotify_app/common/widgets/button/basic_app_button.dart';
+import 'package:spotify_app/core/configs/assets/app_images.dart';
+import 'package:spotify_app/core/configs/assets/app_vectors.dart';
+import 'package:spotify_app/presentation/auth/pages/signin.dart';
+import 'package:spotify_app/presentation/auth/pages/signup.dart';
+
+import '../../../common/widgets/appbar/app_bar.dart';
+import '../../../core/configs/theme/app_colors.dart';
+
+class SignupOrSigninPage extends StatelessWidget {
+  const SignupOrSigninPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          const BasicAppBar(),
+          Align(
+            alignment: Alignment.topRight,
+            child: SvgPicture.asset(AppVectors.topPattern),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SvgPicture.asset(AppVectors.bottomPattern),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Image.asset(AppImages.authBG),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(AppVectors.logo),
+                  const SizedBox(height: 55),
+                  const Text(
+                    'Enjoy Listening To Music',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Spotify is a digital music service that gives you access to millions of songs.',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.grey,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 40),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child:
+                            BasicAppButton(onPressed: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => const SignupPage()));
+                            }, title: 'Register'),
+                      ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        flex: 1,
+                        child: TextButton(
+                            onPressed: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => const SigninPage()));
+                            },
+                            child: Text(
+                              'Sign in',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: context.isDarkMode ? Colors.white : Colors.black
+                              ),
+                            )),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
