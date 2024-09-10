f09a1ce193ae4ee9c3b76ede60aba4a2cda66fac
Maxime Biechy <biechymaxime@gmail.com>
2024-09-10 23:27:51 +0200

    - Add : Urls pour accéder aux covers des songs sur firebase

---
+++ lib/core/configs/constants/app_urls.dart
@@ -0,0 +1,5 @@
+class AppUrls {
+  static const coverFirestorage = 'https://firebasestorage.googleapis.com/v0/b/spotify-app-0101.appspot.com/o/covers%2F';
+  static const medialAlt = 'alt=media';
+  static const defaultImage = 'https://cdn-icons-png.flaticon.com/512/10542/10542486.png';
+}
