03fed94d925a33f5dcee1ab74f8d89227652ee34
Maxime Biechy <biechymaxime@gmail.com>
2024-09-10 23:26:11 +0200

    - Update : Services pour récuperer les songs

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,15 +1,23 @@
 import 'package:get_it/get_it.dart';
 import 'package:spotify_app/data/repository/auth/auth_repository_impl.dart';
 import 'package:spotify_app/data/sources/auth/auth_firebase_service.dart';
+import 'package:spotify_app/data/sources/song/song_firebase_service.dart';
 import 'package:spotify_app/domain/repository/auth/auth.dart';
 import 'package:spotify_app/domain/usecases/auth/signin.dart';
 import 'package:spotify_app/domain/usecases/auth/signup.dart';
+import 'package:spotify_app/domain/usecases/song/get_news_songs.dart';
+
+import 'data/repository/song/song_repository_impl.dart';
+import 'domain/repository/song/song.dart';
 
 final sl = GetIt.instance;
 
 Future<void> initializeDependecies() async {
   sl.registerSingleton<AuthFirebaseService>(AuthFirebaseServiceImpl());
+  sl.registerSingleton<SongFirebaseService>(SongFirebaseServiceImpl());
   sl.registerSingleton<AuthRepository>(AuthRepositoryImpl());
+  sl.registerSingleton<SongRepository>(SongRepositoryImpl());
   sl.registerSingleton<SignUpUseCase>(SignUpUseCase());
   sl.registerSingleton<SignInUseCase>(SignInUseCase());
+  sl.registerSingleton<GetNewsSongsUseCase>(GetNewsSongsUseCase());
 }
